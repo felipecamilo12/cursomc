7b5f195367237c578b4efcae7498924260fba1cb
felipecamilo12 <felipe.camilo.botucatu@gmail.com>
2021-03-05 06:59:12 -0300

    Cliente, TipoCliente, telefones e enderecos

---
+++ src/main/java/com/felipecamilo/cursomc/repositories/EnderecoRepository.java
@@ -0,0 +1,11 @@
+package com.felipecamilo.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.felipecamilo.cursomc.domain.Endereco;
+
+@Repository
+public interface EnderecoRepository extends JpaRepository<Endereco, Integer> {
+
+}
